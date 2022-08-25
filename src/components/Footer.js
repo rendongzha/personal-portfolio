42f80478f3e5f4c15d5d963076faa7e263a821cb
katherinegreen <rendong0809@gmail.com>
2022-08-25 15:14:58 +1000

    add copyright sign

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>Copyright 2022</p>
+      <p>&copy; Copyright 2022</p>
       <p>Created my logo at LogoMakr.com/app</p>
     </footer>
   );
