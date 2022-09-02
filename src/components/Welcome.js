cabad8cc483b980fd330d0e98d4dd1c90b4e53d7
katherinegreen <rendong0809@gmail.com>
2022-09-02 21:31:38 +1000

    add skills

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,8 @@ const Welcome = () => {
       <h1>Hey I am Katherine</h1>
       <p>Targeted position: front-end developer</p>
       <p className={classes.skillset}>
-        Skillset: HTML, CSS, JavaScript, React.js, Redux.js, Git
+        Skillset: HTML, CSS, JavaScript, React.js, Redux.js,TypeScript, Sass,
+        Git
       </p>
     </section>
   );
